9c72285a9f1d61d4c76cc6f45c7df274756b0801
battsun <45926244+battsun@users.noreply.github.com>
2019-02-25 18:45:23 +0900

    first commit

---
+++ LedRaw_AdaNeoPixel.cpp
@@ -0,0 +1,255 @@
+#include "LedRaw_AdaNeoPixel.h"
+#include <Adafruit_NeoPixel.h>
+#include <StandardCplusplus.h>
+#include <vector>
+using namespace std;
+
+//constructor
+LedRaw_AdaNeoPixel::LedRaw_AdaNeoPixel(void){
+  LedRaw_AdaNeoPixel(6,1,10,SINGLE);
+}
+
+LedRaw_AdaNeoPixel::LedRaw_AdaNeoPixel(int pinNum, uint8_t lane, uint8_t row, LedBarType mode){
+  mLane=lane;
+  mRow=row;
+  mBarType=mode;
+  mLeds=mLane*mRow*mBarType;
+
+  mStrip=Adafruit_NeoPixel(mLeds, pinNum, NEO_GRB + NEO_KHZ800);
+  mLedMatrix =vector<vector<uint8_t>>(mLane, vector<uint8_t>(mRow, 0));
+  mLedMatrixP=vector<vector<uint8_t>>(mLane, vector<uint8_t>(mRow, 0));
+  mPatternNum=0;
+
+  for(uint8_t i=0;i<mLane;i++){
+    for(uint8_t j=0;j<mRow;j++){
+      if(mBarType==SINGLE){
+        mLedMatrix[i][j]=i*mRow+j;
+      } else{
+         mLedMatrix[i][j]=2*i*mRow+j;
+        mLedMatrixP[i][j]=2*(i+1)*mRow-j-1;
+      }
+    }
+  }
+
+  //プリセット色の初期化
+  red  =ColorUnionInit(255,0,0);
+  green=ColorUnionInit(0,255,0);
+  white=ColorUnionInit(128,128,128);
+  blue =ColorUnionInit(0,0,255);
+}
+
+//destructor
+LedRaw_AdaNeoPixel::~LedRaw_AdaNeoPixel(){
+}
+
+void LedRaw_AdaNeoPixel::begin(void){
+  mStrip.begin();
+}
+
+void LedRaw_AdaNeoPixel::setBrightness(uint8_t br){  
+  mStrip.setBrightness(br);
+}
+
+//NeoPixel.SHOW()
+void LedRaw_AdaNeoPixel::show(void){
+  mStrip.show();
+}
+
+//set one led color
+void LedRaw_AdaNeoPixel::setColor(uint8_t lane, uint8_t row,int g, int r, int b){
+  switch(mBarType){
+    case SINGLE:
+      mStrip.setPixelColor( mLedMatrix[lane][row],g,r,b);
+    break;
+    case PAIR:
+      mStrip.setPixelColor( mLedMatrix[lane][row],g,r,b);
+      mStrip.setPixelColor(mLedMatrixP[lane][row],g,r,b);
+    break;
+    default:
+    break;
+  }
+}
+
+//set one led white level 
+void LedRaw_AdaNeoPixel::setColorW(uint8_t lane, uint8_t row,int br){
+  setColor(lane, row,br,br,br);
+}
+
+//create color
+col LedRaw_AdaNeoPixel::ColorUnionInit(uint8_t r,uint8_t g,uint8_t b){
+  col col_;
+  col_.zero=0;col_.r=r;col_.g=g;col_.b=b;
+  return col_;
+}
+
+////////////////////////////////////////////////////////////////////////////////  
+///////////////   pattern    /////////////////////////////////////////////////// 
+//////////////////////////////////////////////////////////////////////////////// 
+
+//////////////////////////////////////// 
+//SnowFall
+void LedRaw_AdaNeoPixel::patternSnowFallInit(void){
+uint8_t TAILTABLE[40]={255,192,168,144,121,96,64,32,16,8,4,2,2,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+int tailLen=40;
+mPatternNum++;
+
+  for(uint8_t i=0;i<tailLen;i++){
+    mSnowFallTailAry[i]=TAILTABLE[i];
+  }
+  mSnowFallTailLen=tailLen;
+
+  mSnowFallTopIndex=vector<uint8_t>(mLane);
+  for(int i=0;i<mLane;i++){
+    mSnowFallTopIndex[i]=random( mSnowFallTailLen);
+  }
+}
+
+void LedRaw_AdaNeoPixel::patternSnowFall(void){
+   // forward the first led's status one step on each Bar
+  for(int i=0;i<mLane;i++){
+    mSnowFallTopIndex[i]=(mSnowFallTopIndex[i]+1) % mSnowFallTailLen;
+  }
+
+  //Change all leds color
+  for(uint8_t i=0;i<mLane;i++){
+    for(uint8_t j=0;j<mRow;j++){
+      uint8_t br=mSnowFallTailAry[(mSnowFallTopIndex[i]+mRow-j) % mSnowFallTailLen];
+      setColorW(i,j,br);
+    }
+  }
+}
+
+/////////////////////////////////////
+//二色の間の点滅パターン
+void LedRaw_AdaNeoPixel::patternBlink2ColorInit(uint16_t step,col col1,col col2){
+  mBlink2ColorStep=step;
+  mBlink2ColorCol1=col1;
+  mBlink2ColorCol2=col2;
+  mPatternNum++;
+
+}
+
+void LedRaw_AdaNeoPixel::patternBlink2Color(void){
+  int r=(millis()-mReferenceTime) % (mBlink2ColorStep *4);
+  int c=constrain(abs(r-5 * mBlink2ColorStep / 2) - mBlink2ColorStep/2, 0, mBlink2ColorStep);
+
+  //全LEDについて書き換え
+  for(uint8_t i=0;i<mLane;i++){
+    for(uint8_t j=0;j<mRow;j++){
+      if((i+j)%2==0){
+        setColor(i,j,map(c,0,mBlink2ColorStep,mBlink2ColorCol1.g,mBlink2ColorCol2.g),
+                     map(c,0,mBlink2ColorStep,mBlink2ColorCol1.r,mBlink2ColorCol2.r),
+                     map(c,0,mBlink2ColorStep,mBlink2ColorCol1.b,mBlink2ColorCol2.b));
+      } else{
+        setColor(i,j,map(c,0,mBlink2ColorStep,mBlink2ColorCol2.g,mBlink2ColorCol1.g),
+                     map(c,0,mBlink2ColorStep,mBlink2ColorCol2.r,mBlink2ColorCol1.r),
+                     map(c,0,mBlink2ColorStep,mBlink2ColorCol2.b,mBlink2ColorCol1.b));
+      }
+    }
+  }
+}
+
+/////////////////////////////////////
+//氷と雨くるくるパターン
+void LedRaw_AdaNeoPixel::patternFalling2ColorInit(uint16_t period, col col1, col col2){
+  mFalling2ColorPeriod=period;
+  mFalling2ColorCol1=col1;
+  mFalling2ColorCol2=col2;
+  mPatternNum++;
+}
+
+void LedRaw_AdaNeoPixel::patternFalling2Color(void){
+  //周期は4LEDなので、
+  int c=((millis()-mReferenceTime) % (mFalling2ColorPeriod*2)) / mFalling2ColorPeriod;
+  
+  //全LEDについて書き換え
+  for(uint8_t i=0;i<mLane;i++){
+    for(uint8_t j=0;j<mRow;j++){
+      switch((i + mRow - j + c) % 4){
+        case 0:
+        setColor(i,j,mFalling2ColorCol1.g,mFalling2ColorCol1.r,mFalling2ColorCol1.b);
+        break;
+        case 1:
+        setColor(i,j,mFalling2ColorCol2.g,mFalling2ColorCol2.r,mFalling2ColorCol2.b);
+        break;
+        default:
+        break;
+      }
+    }
+  }
+}
+
+/////////////////////////////////////
+//光がらせんを飛ぶパターン
+void LedRaw_AdaNeoPixel::patternFlyingLightSpiralInit(){
+  mSpiralVx=1;
+  mSpiralVy=0.4;
+  mSpiralPeriod=100;
+}
+
+void LedRaw_AdaNeoPixel::patternFlyingLightSpiral(){
+
+  //LEDバーを座標系に見立てて光を飛ばす
+  float t=(float)millis() / mSpiralPeriod;
+  int lightx[mSpiralN];
+  int lighty[mSpiralN];
+  lightx[0]=(int)(mSpiralVx*t) % mLane;
+  lighty[0]=(int)(mSpiralVy*t) % mRow;
+  for(int i=1;i<mSpiralN;i++){
+    lightx[i]=(lightx[i-1]+1)% mLane;
+    lighty[i]=lighty[i-1]+0;
+  }
+
+  //全LEDについてクリア
+  for(uint8_t ip=0;ip<mLane;ip++){
+    for(uint8_t j=0;j<mRow;j++){
+      setColorW(ip,j,0);
+    }
+  }
+  //光粒の座標を点灯させる
+  for(int i=0;i<mSpiralN;i++){
+    setColorW(lightx[i],lighty[i],4+i*24);
+  }
+
+}
+
+/////////////////////////////////////
+//Star Run
+void LedRaw_AdaNeoPixel::patternStarrunInit(uint16_t per=100){
+  mStarrunCycleN=15;
+  mStarrunPeriod=per;
+  mPatternNum++;
+}
+
+void LedRaw_AdaNeoPixel::patternStarrun(){
+  int c=((millis()-mReferenceTime) % (mStarrunPeriod * mStarrunCycleN)) / mStarrunPeriod;
+
+  //全LEDについてクリア
+  for(uint8_t i=0;i<mLane;i++){
+    for(uint8_t j=0;j<mRow;j++){
+      setColorW(i,j,0);
+    }
+  }
+  setColorW(0,mStarrunAry[c],128);
+}
+
+/////////////////////////////////////////////////////////////////////
+// Under development
+/////////////////////////////////////////////////////////////////////
+/* void LedRaw_AdaNeoPixel::addPatternInit(void (*pf)(uint32_t[])){
+  patternInitTable->emplace_back((*pf)(uint32_t[]));
+}
+
+void LedRaw_AdaNeoPixel::execPatternInit(int i, uint32_t arg[]){
+  patternInitTable[i](arg[]);
+} 
+
+void LedRaw_AdaNeoPixel::addPattern(voidfunc f){
+  patternTable->emplace_back(f);
+}
+
+void LedRaw_AdaNeoPixel::execPattern(int i){
+  patternTable[i]();
+}
+*/
+
---
+++ LedRaw_AdaNeoPixel.h
@@ -0,0 +1,114 @@
+#ifndef LEDRAW_ADANEOPIXEL_H
+#define LEDRAW_ADANEOPIXEL_H
+
+#include <Adafruit_NeoPixel.h>
+#include <StandardCplusplus.h>
+#include <vector>
+using namespace std;
+
+typedef union {
+  struct{
+    uint8_t zero;
+    uint8_t r;
+    uint8_t g;
+    uint8_t b;
+  } ; //構造体名もタグ名も省略すると、col.tag_.zero　といった表記を縮めて col.zeroとできる　？
+  uint32_t rgb;
+} col;
+
+typedef void (*voidfunc)();
+
+enum LedBarType{
+  SINGLE=1,
+  PAIR=2
+};
+ 
+
+class LedRaw_AdaNeoPixel{
+	public:
+  LedRaw_AdaNeoPixel(void);
+  LedRaw_AdaNeoPixel(int pinNum, uint8_t lane, uint8_t row, LedBarType mode);
+  ~LedRaw_AdaNeoPixel(void);
+  void show(void);
+  void begin();
+  void setBrightness(uint8_t br);
+  void setReferenceTime(void){mReferenceTime=millis();};
+  uint8_t getPatternNum(){return mPatternNum;};
+  long getReferenceTime(){return mReferenceTime;};
+
+  //lighting patterns
+  void patternSnowFall(void);
+  void patternSnowFallInit(void);
+  void patternBlink2Color(void);
+  void patternBlink2ColorInit(uint16_t step, col col1, col col2);
+  void patternFalling2Color(void);
+  void patternFalling2ColorInit(uint16_t period, col col1, col col2);
+  void patternStarrun(void);
+  void patternStarrunInit(uint16_t per);
+  void patternFlyingLightSpiral(void);
+  void patternFlyingLightSpiralInit(void);
+
+  //uint8_t TAILTABLE[TAILLEN]={255,192,144,96,64,32,8,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+
+  //preset colors
+  col red; // static col red;　とするとき、cppの方にグローバル変数として col LedRaw_AdaNeoPixel::red; とかく。ヘッダに書くと二重定義になる
+  col green;
+  col white;
+  col blue;
+
+
+  protected:             
+  
+  private:  
+  Adafruit_NeoPixel mStrip;
+  uint8_t mLane;
+  uint8_t mRow;
+  LedBarType mBarType;
+  uint8_t mLeds; 	//LEDの総数
+  vector<vector<uint8_t>> mLedMatrix;
+  vector<vector<uint8_t>> mLedMatrixP;
+
+  uint8_t mPatternNum;
+  long mReferenceTime;
+  void setColor (uint8_t lane, uint8_t row,int g, int r, int b);
+  void setColorW(uint8_t lane, uint8_t row,int br);
+  col ColorUnionInit(uint8_t r,uint8_t g,uint8_t b);
+
+  //void (*patternTable)()[10];
+  /* vector<void> (*patternInitTable)(uint32_t[]);
+  void addPatternInit(void (*pf)(uint32_t[]));
+  void execPatternInit(int);
+  vector<voidfunc> patternTable;
+  void addPattern(voidfunc);
+  void execPattern(int);
+ */  
+
+  //パターン１　スノーフォール
+  uint8_t* mSnowFallTailAry;
+  uint8_t mSnowFallTailLen;
+  vector<uint8_t> mSnowFallTopIndex;
+
+  //パターン2 ２色フェーディング
+  col mBlink2ColorCol1;
+  col mBlink2ColorCol2;
+  long mBlink2ColorStep;
+
+  //パターン3 ２色が落ちる
+  col mFalling2ColorCol1;
+  col mFalling2ColorCol2;
+  int mFalling2ColorPeriod;
+
+  //パターン4 starrun
+  uint8_t mStarrunCycleN;
+  vector<uint8_t> mStarrunAry = {0,1,3,4,5,7,8,9,1,2,3,5,6,7,9};
+  uint16_t mStarrunPeriod;
+
+  //パターン5 spiralLight
+  float mSpiralVx;
+  float mSpiralVy;
+  uint8_t mSpiralPeriod;
+  const uint8_t mSpiralN=4;//　光の粒の数
+
+};
+
+#endif
